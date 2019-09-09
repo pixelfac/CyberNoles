1e219e94a1f68a018b6cd071984c58649ca00d02
pixel <43653484+pixelfac@users.noreply.github.com>
2019-09-08 20:28:56 -0500

    Update TeleOpS2.java
    fixed the bugs/errors with last version.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpS2.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpS2.java
@@ -7,8 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@TeleOp(name = "TeleOpS2", group = "T3")
 public class TeleOpS2 extends LinearOpMode {
-
+    private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
@@ -27,29 +28,45 @@ public class TeleOpS2 extends LinearOpMode {
     }
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
+
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBackDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+
+        telemetry.addData(">", "Press Start To Run TeleOp");
+        telemetry.update();
+
+        waitForStart();
 
         while (opModeIsActive()) {
 
-            if (gamepad1.left_stick_y >= 0.05)
+            if (gamepad1.left_stick_x >= 0.5)
                 drive = 1;
-            else if (gamepad1.left_stick_y <= 0.05)
+            else if (gamepad1.left_stick_x <= -0.5)
                 drive = -1;
+            else drive = 0;
 
-            if (gamepad1.left_stick_x >= 0.05)
+            if (gamepad1.left_stick_y <= -0.5)
                 turn = 1;
-            else if (gamepad1.left_stick_x <= 0.05)
+            else if (gamepad1.left_stick_y >= 0.5)
                 turn = -1;
+            else turn = 0;
 
-            if (gamepad1.right_stick_x >= 0.05)
+            if (gamepad1.right_stick_x >= 0.5)
                 strafe = 1;
-            else if (gamepad1.right_stick_x <= 0.05)
+            else if (gamepad1.right_stick_x <= -0.5)
                 strafe = -1;
+            else strafe = 0;
+
+
 
             leftFrontDrive.setPower(limit(drive + turn + strafe));
             leftBackDrive.setPower(limit(drive + turn - strafe));
             rightFrontDrive.setPower(limit(drive - turn + strafe));
             rightBackDrive.setPower(limit(drive - turn - strafe));
+
 
 
 
