54f8dd6071766fabb97968e628ff8f3cfbec9d49
pixel <43653484+pixelfac@users.noreply.github.com>
2019-10-05 14:25:57 -0500

    Update to TeleOpS2
    added untested functionality to scissor lift and grabbing joint. Also corrected typo in Drive train so it should read consistently now

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpS2.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpS2.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -14,6 +15,9 @@ public class TeleOpS2 extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+    private DcMotor scislift = null;
+    private CRServo grabber = null;
+
 
     private double drive = 0;
     private double turn = 0;
@@ -35,24 +39,33 @@ public class TeleOpS2 extends LinearOpMode {
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
 
+        scislift = hardwareMap.get(DcMotor.class, "scislift");
+        grabber = hardwareMap.get(CRServo.class, "grabber");
+
         telemetry.addData(">", "Press Start To Run TeleOp");
         telemetry.update();
+
+        //when the scissorlift motor is set to zero, it will resist rotating
+        //so as to hold the lift in an elevated position
+        scislift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
         while (opModeIsActive()) {
 
+            //Drivetrain movement
+            //left stick for drive and turn, right stick for strafe
             if (gamepad1.left_stick_x >= 0.5)
-                drive = 1;
-            else if (gamepad1.left_stick_x <= -0.5)
-                drive = -1;
-            else drive = 0;
-
-            if (gamepad1.left_stick_y <= -0.5)
                 turn = 1;
-            else if (gamepad1.left_stick_y >= 0.5)
+            else if (gamepad1.left_stick_x <= -0.5)
                 turn = -1;
             else turn = 0;
+
+            if (gamepad1.left_stick_y <= -0.5)
+                drive = 1;
+            else if (gamepad1.left_stick_y >= 0.5)
+                drive = -1;
+            else drive = 0;
 
             if (gamepad1.right_stick_x >= 0.5)
                 strafe = 1;
@@ -64,12 +77,47 @@ public class TeleOpS2 extends LinearOpMode {
 
             leftFrontDrive.setPower(limit(drive + turn + strafe));
             leftBackDrive.setPower(limit(drive + turn - strafe));
-            rightFrontDrive.setPower(limit(drive - turn + strafe));
-            rightBackDrive.setPower(limit(drive - turn - strafe));
+            rightFrontDrive.setPower(limit(- drive + turn + strafe));
+            rightBackDrive.setPower(limit( - drive + turn - strafe));
 
 
+            //scissorlift Movement
+            //DPAD up and down to raise and lower the lift
+            if (gamepad2.dpad_up)
+                scislift.setPower(0.5);
+            else if (gamepad2.dpad_down)
+                scislift.setPower(-0.5);
+            else
+                scislift.setPower(0);
 
+            //grabber movement
+            if (gamepad2.right_stick_y > 0.2)
+                grabber.setPower(0.3);
+            else if (gamepad2.right_stick_y < -0.2)
+                grabber.setPower(-0.3);
+            else
+                grabber.setPower(0);
 
         }
     }
 }
+
+/*
+leftx = drive
+lefty = turn
+rightx = strafe
+
+d + t + s
+d + t - s
+d - t + s
+d - t - s
+
+
+leftx = turn
+lefty = drive
+rightx = strafe
+
+
+
+
+*/
